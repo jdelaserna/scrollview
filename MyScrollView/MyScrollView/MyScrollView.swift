9b91501493e1e594f66828874b1a4fbecb912d29
Jorge <jorgeserna94@gmail.com>
2018-07-20 13:20:10 -0700

    Add files via upload

---
+++ MyScrollView/MyScrollView/MyScrollView.swift
@@ -0,0 +1,13 @@
+//
+//  MyScrollView.swift
+//  MyScrollView
+//
+//  Created by Jorge Serna on 2018-06-11.
+//  Copyright © 2018 Jorge Serna. All rights reserved.
+//
+
+import UIKit
+
+class MyScrollView: UIView {
+    
+}
---
+++ MyScrollView/MyScrollView/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MyScrollView
+//
+//  Created by Jorge Serna on 2018-06-08.
+//  Copyright © 2018 Jorge Serna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()        
+    }
+
+}
+
